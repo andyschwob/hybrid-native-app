57cb68ee195592dd143bd00771c57ca320434ddc
Dan Rudolf <dan.rudolf@brandingbrand.com>
2023-01-30 11:05:06 -0600

    wip: adding android

---
+++ android/app/src/main/java/com/brandingbrand/testtabapp/ui/main/PageViewModel.kt
@@ -0,0 +1,19 @@
+package com.brandingbrand.testtabapp.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class PageViewModel : ViewModel() {
+
+    private val _index = MutableLiveData<Int>()
+    val text: LiveData<String> = Transformations.map(_index) {
+        "Fragment $it"
+    }
+
+    fun setIndex(index: Int) {
+        _index.value = index
+    }
+}
---
+++ android/app/src/main/java/com/brandingbrand/testtabapp/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,37 @@
+package com.brandingbrand.testtabapp.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.brandingbrand.testtabapp.R
+
+private val TAB_TITLES = arrayOf(
+        R.string.tab_text_1,
+        R.string.tab_text_2,
+        R.string.tab_text_3
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
+    : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        while(position < 2) {
+            return PlaceholderFragment.newInstance(position + 1)
+        }
+        return ReactNativeFragment.newInstance()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 3
+    }
+}
