0faccc17a81237235c46cb642fad9036cd1e1126
Andy Schwob <andy.schwob@brandingbrand.com>
2023-03-02 10:51:37 -0500

    fix: update packages and apply patch for snap carousel

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-import React from "react";
-import { AppRegistry, NativeEventEmitter, NativeModules, StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState, useEffect } from "react"
+import EngagementLite, { EngagementCompGhost } from '@brandingbrand/fsengagementlite';
+import { AppRegistry, NativeEventEmitter, NativeModules, StyleSheet, View } from "react-native";
 
 const { BridgeModule, BridgeModuleEventEmitter } = NativeModules;
 
-const onUpdateProfileAttributes = (profileAttributes) => {
-  console.log("ATTRIBUTES RECEIVED: " + profileAttributes);
-}
+const handleDeepLink = link => {
+  // Dispatch deep link to receiving application.
+  BridgeModule.dispatchDeepLinkEvent({
+    component: "ComponentOne",
+    url: link
+  });
+};
 
-const ReactNativeTest = () => {
-  const eventEmitter = new NativeEventEmitter(BridgeModuleEventEmitter);
+const handleNavigateToPost = data => {
+  const { postId, json, title, dynamicData, path } = data;
+  //Dispatch show post event to receiving application.
+  BridgeModule.dispatchShowPostEvent({
+    component: "ComponentOne",
+    data: {
+      postId,
+      json,
+      name: title,
+      dynamicData
+    }
+  });
+};
 
-  eventEmitter.addListener("onUpdateProfileAttributes", onUpdateProfileAttributes);
+const handleBackPress = () => {
   // Dispatch for application exit so the receiving native code can handle navigation.
   BridgeModule.dispatchExitEvent({
     component: "ComponentOne",
   });
-
-  // Dispatch deep link to receiving application. 
-  BridgeModule.dispatchDeepLinkEvent({
-    component: "ComponentOne",
-    link: "/discover/messageid"
-  });
-
-  //Dispatch show post event to receiving application.
-  BridgeModule.dispatchShowPostEvent({
-    component: "ComponentOne",
-    name: "",
-    postId: "1337",
-    json: " ",
-    dynamicData: { }
-  });
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>React Native Test</Text>
-    </View>
-  );
 };
+
+const { engagementService, EngagementComp } = EngagementLite({
+  appId: 'f547cb88-d59b-4edb-8ae9-f067c1cc5010', // Core example app (Hybrid Native App)
+  baseURL: 'https://api.brandingbrand.com/engagement-general/v1',
+  cacheTTL: 0,
+  handleDeepLink,
+  handleNavigateToPost,
+  handleBackPress
+});
+
 var styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,4 +53,98 @@ var styles = StyleSheet.create({
     margin: 10,
   },
 });
-AppRegistry.registerComponent("ComponentOne", () => ReactNativeTest);
+
+const EngagementFeed = () => {
+
+  const eventEmitter = new NativeEventEmitter(BridgeModuleEventEmitter);
+
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [attributesArr, setAttributesArr] = useState([]);
+
+  const fetchEngagementInbox = async () => {
+    await engagementService.getProfile(); // add user info?
+    const attributes = JSON.stringify(attributesArr);
+  
+    const inboxResponse = await engagementService.getInboxMessages({ attributes });
+    const sorted = await engagementService.sortInbox(inboxResponse);
+    return sorted.map(inboxPost => (inboxPost.message));
+  } 
+
+  const fetchDiscover = useCallback(async () => {
+    await fetchEngagementInbox()
+      .then(posts => {
+        setLoading(false);
+        setPosts(
+          posts.map(post => ({
+            id: post.id,
+            name: post.title,
+            ...post.content
+          }))
+        );
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, [setLoading, setPosts]);
+
+  useEffect(() => {
+    fetchDiscover().catch(e => console.error(e));
+  }, []);
+
+  eventEmitter.addListener("onUpdateProfileAttributes", profileAttributes => {
+    // example attributes object:
+    // {
+    //   gender: 'male',
+    //   age: 21,
+    //   country_code: 'US',
+    //   isLoggedIn: true
+    // }
+    const attr = [];
+    for (const [key, value] of Object.entries(profileAttributes)) {
+      attr.push({
+        key,
+        value,
+        type: typeof value || 'string'
+      });
+    }
+    setAttributesArr(attr);
+    fetchDiscover().catch(e => console.error(e));
+  });
+
+  const json = { private_type: 'feed', private_blocks: posts };
+
+  if (loading) {
+    return <EngagementCompGhost />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <EngagementComp
+        refreshControl={fetchDiscover}
+        isLoading={false}
+        json={json}
+      />
+    </View>
+  );
+};
+
+
+const EngagementStory = (props) => {
+  const { json, backButton = false, name, id } = props;
+
+  return (
+    <View style={styles.container}>
+      <EngagementComp
+        isLoading={false}
+        json={json}
+        backButton={backButton}
+        name={name}
+        id={id}
+      />
+    </View>
+  );
+};
+
+AppRegistry.registerComponent("ComponentOne", () => EngagementFeed);
+AppRegistry.registerComponent("StoryComponent", () => EngagementStory);
